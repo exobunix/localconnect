026a3492a8bf9cfc3d58e576f34d8a6eff221771
adarshsachan7071 <adarshsachan7071@gmail.com>
2026-08-29 21:34:10 +0530

    chore: disable mergeReleaseNativeDebugMetadata task to avoid symbol extraction failure on Windows

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -84,3 +84,10 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.concurrent:concurrent-futures:1.3.0")
 }
+
+tasks.configureEach {
+    if (name.startsWith("merge") && name.contains("NativeDebugMetadata")) {
+        enabled = false
+    }
+}
+
